0ac777a8872600fe1c1ba90b3627adc76f5c4177
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-09-27 20:10:41 +0700

    initial commit

---
+++ StrollOne/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  StrollOne
+//
+//  Created by Ashraf Alif Adillah on 27/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StrollOne/StrollOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollOneApp.swift
+//  StrollOne
+//
+//  Created by Ashraf Alif Adillah on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
